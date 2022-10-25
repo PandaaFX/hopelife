196490708c52299923168ee5ca6c967a1185488b
PandaaFX <kleo.minecraft@yahoo.com>
2022-10-25 15:21:14 +0200

    123

---
+++ updater/update.js
@@ -0,0 +1,14 @@
+const AutoGitUpdate = require('auto-git-update');
+
+const config = {
+    repository: 'https://github.com/PandaaFX/hopelife',
+    token: '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',
+    tempLocation: 'C:/Users/Administrator/Desktop/pandaatmp/',
+    branch: 'main',
+    fromReleases: false,
+    exitOnComplete: true
+}
+
+const updater = new AutoGitUpdate(config);
+
+updater.autoUpdate();
