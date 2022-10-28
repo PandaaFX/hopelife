2540fc68b068c27ce64b9a11aa3d3134db90e2d1
PandaaFX <86691131+PandaaFX@users.noreply.github.com>
2022-10-28 11:46:36 +0200

    Halloween Loadingscreen

---
+++ [Pandaa]/halloween-loadingscreen/__resource.lua
@@ -0,0 +1,11 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+loadscreen "html/index.html"
+
+files {
+    "html/*",
+	"html/audio.js",
+	"html/0_big.png",
+	"html/new-song_old.mp3",
+	"html/fonts/*.ttf"
+}
