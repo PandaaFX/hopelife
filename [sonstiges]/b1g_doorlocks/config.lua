5eb74e4e6895f25b0b6f211ec2682f42b2f9aaf1
tazlash <github@tazlash.com>
2022-11-05 14:39:34 +0100

    airport door

--- [sonstiges]/b1g_doorlocks/config.lua
+++ [sonstiges]/b1g_doorlocks/config.lua
@@ -1,26 +1,20 @@
 Config = {}
 Config.DoorList = {
 	{
-		distance = 2.5,
+		distance = 2,
 		objName = -545008322,
 		objHeading = 149.99998474121,
 		objCoords = {x=-1042.154,y=-2748.143,z=20.33327},
-		lockpick = false,
 		authorizedJobs = { ['admin']=0 },
-		locked = true,
-		slides = false,
-		size = 0.2
+		locked = true
 	},
 	{
-		distance = 2.5,
+		distance = 2,
 		objName = -545008322,
 		objHeading = 330.00006103516,
 		objCoords = {x=-1044.839,y=-2746.593,z=20.33327},
-		lockpick = false,
 		authorizedJobs = { ['admin']=0 },
-		locked = true,
-		slides = false,
-		size = 0.2
+		locked = true
 	}
 }
 
