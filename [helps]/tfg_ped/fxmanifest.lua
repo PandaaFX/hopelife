afc4e748ee7eccc14b6e8ed98f899f1c8697c4c5
Chillaxo <Jojo.fcb@gmx.de>
2022-11-06 03:55:58 +0100

    Npc fix + features :)

---
+++ [helps]/tfg_ped/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+game 'gta5'
+
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+
+author 'Ch1ll4x0'
+
+Description 'ped'
+version '0.0.1'
+
+shared_script 'config.lua'
+
+client_scripts { 
+    'client.lua'
+}
+server_scripts {
+    'server.lua'
+} 
+
+lua54 'yes'
